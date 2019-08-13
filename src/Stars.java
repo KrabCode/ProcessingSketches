6673917eca1114a49024086967cb6a92883a3e11
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-08-13 19:51:18 +0200

    stars

---
+++ src/Stars.java
@@ -0,0 +1,77 @@
+import applet.GuiSketch;
+import peasy.PeasyCam;
+import processing.core.PVector;
+
+import java.util.ArrayList;
+
+public class Stars extends GuiSketch {
+    ArrayList<Star> stars = new ArrayList<Star>();
+    PeasyCam cam;
+    int bgDark;
+    private final float depth = 2000;
+
+    public static void main(String[] args) {
+        GuiSketch.main("Stars");
+    }
+
+    public void settings() {
+        size(960, 800, P3D);
+    }
+
+    public void setup() {
+        super.setup();
+        bgDark = color(62, 68,70);
+
+        cam = new PeasyCam(this, depth);
+    }
+
+    public void draw() {
+        super.draw();
+        background(bgDark);
+        float t = radians(frameCount);
+        updateDrawStars(t);
+        if (frameCount > 5 && frameCount <= 365) {
+            saveFrame(captureDir + "####.jpg");
+        }
+        gui();
+    }
+
+    private void updateDrawStars(float t) {
+        int starCount = 5000;
+        while (stars.size() < starCount) {
+            stars.add(new Star());
+        }
+        for (Star s : stars) {
+            s.update(t);
+        }
+    }
+
+    class Star {
+        PVector pos;
+        float weight = 1 + abs(randomGaussian());
+
+        private float fadeInDuration = 30;
+        float fadeInStarted = -fadeInDuration * 2;
+        private float lastZ;
+
+        Star() {
+            float x = randomGaussian() * width;
+            float y = randomGaussian() * width;
+            pos = new PVector(x, y, random(depth));
+            lastZ = pos.z;
+        }
+
+        void update(float t) {
+            float timeOffset = map(t, 0, TWO_PI, 0, depth);
+            float z = (pos.z + timeOffset) % depth;
+            if (lastZ > z) {
+                fadeInStarted = frameCount;
+            }
+            lastZ = z;
+            float fadeInNormalized = map(frameCount, fadeInStarted, fadeInStarted + fadeInDuration, 0, 1);
+            stroke(lerpColor(bgDark, color(255), fadeInNormalized));
+            strokeWeight(weight);
+            point(pos.x, pos.y, z);
+        }
+    }
+}
