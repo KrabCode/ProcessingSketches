b443707a1ab19ba9f097e502c1ffe0cb779870b9
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-08-26 15:00:38 +0200

    mandelbrot improved

--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -1,4 +1,5 @@
 import applet.GuiSketch;
+import processing.core.PGraphics;
 import processing.core.PVector;
 import processing.event.MouseEvent;
 import processing.opengl.PShader;
@@ -10,13 +11,18 @@ public class Mandelbrot extends GuiSketch {
     // TODO also try motion blur maybe
 
     private PShader mandelbrot;
-    private PVector offset;
+    private PShader blur;
+    private PVector cameraPos;
+
     private float scale = 1;
+    private float scaleTarget = 1;
+
     float t;
     private boolean recording;
     private float recStart;
     private float recEnd;
     private float recLength;
+    private PGraphics canvas;
 
     public static void main(String[] args) {
         GuiSketch.main("Mandelbrot");
@@ -28,50 +34,53 @@ public class Mandelbrot extends GuiSketch {
     }
 
     public void setup() {
+        canvas = createGraphics(width * 2, height * 2, P2D);
         mandelbrot = loadShader("mandelbrot.glsl");
-        mandelbrot.set("palette", loadImage("palette.png"));
-        offset = new PVector();
+        blur = loadShader("blur.glsl");
+        cameraPos = new PVector();
     }
 
     public void draw() {
-        mandelbrot.set("distortMag", slider("distort",0,.1f, 0));
-        t += radians(1);
-        recLength = 360/slider("t");
-
-        mandelbrot.set("scale", scale);
-        mandelbrot.set("offset", offset.x, offset.y);
-        mandelbrot.set("hueStart",slider("hueStart", 5));
-        mandelbrot.set("hueRange",slider("hueRange", 5));
-
-        float iter = slider("iterStart", 1000) + slider("iterRange", 1000) * sin(t);
+        int timeMultiplier = floor(slider("time mult", .8f, 5));
+        t += radians(1f/timeMultiplier);
+        recLength = 360*timeMultiplier;
         mandelbrot.set("t", t);
-        mandelbrot.set("iter", iter);
-
+        mandelbrot.set("scale", scale);
+        mandelbrot.set("offset", cameraPos.x, cameraPos.y);
+        mandelbrot.set("hueStart", slider("hueStart", 0, 5, .6f));
+        mandelbrot.set("hueRange", slider("hueRange", 0, 5, .1f));
+        mandelbrot.set("distortMag", slider("distort", 0, .1f, 0));
+        mandelbrot.set("iter", map(2*abs(recLength/2-(frameCount%recLength)), 0, recLength, 0, slider("maxIter", 1000)));
+        mandelbrot.set("brLimit", slider("brLimit", 0, 2, 0.2f));
         if (mousePressed) {
-            offset.x += (pmouseX - mouseX)*scale;
-            offset.y -= (pmouseY - mouseY)*scale;
+            cameraPos.x += (pmouseX - mouseX) * scale * (width / (float) height); //assuming width > height
+            cameraPos.y -= (pmouseY - mouseY) * scale;
         }
-        filter(mandelbrot);
+        scale = lerp(scale, scaleTarget, .2f);
 
-        if(recEnd > frameCount){
-            saveFrame(captureDir+"####.jpg");
+        canvas.beginDraw();
+        canvas.filter(mandelbrot);
+        canvas.endDraw();
+        image(canvas, 0, 0, width, height);
+
+        if (recEnd > frameCount) {
+            saveFrame(captureDir + "####.jpg");
         }
 
         gui();
-
     }
 
-    public void keyPressed(){
+    public void keyPressed() {
         recStart = frameCount;
         recEnd = frameCount + recLength;
     }
 
     public void mouseWheel(MouseEvent event) {
         float e = event.getCount();
-        if(e < 0){
-            scale *= .9;
-        }else{
-            scale *= 1.1;
+        if (e < 0) {
+            scaleTarget *= .9;
+        } else {
+            scaleTarget *= 1.1;
         }
     }
 }
--- src/data/mandelbrot.glsl
+++ src/data/mandelbrot.glsl
@@ -6,7 +6,6 @@ precision mediump float;
 precision mediump int;
 #endif
 
-
 uniform sampler2D palette;
 uniform sampler2D texture;
 uniform float t;
@@ -15,36 +14,36 @@ uniform vec2 resolution;
 uniform vec2 offset;
 uniform float iter;
 uniform float scale;
-
+uniform float brLimit;
 uniform float hueStart;
 uniform float hueRange;
 
-
-vec3 rgb( in vec3 c ){
- vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0), 6.0)-3.0)-1.0, 0.0, 1.0 );
- rgb = rgb*rgb*(3.0-2.0*rgb);  return c.z * mix(vec3(1.0), rgb, c.y);
+vec3 rgb(in vec3 c){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0, 4.0, 2.0), 6.0)-3.0)-1.0, 0.0, 1.0);
+    rgb = rgb*rgb*(3.0-2.0*rgb);  return c.z * mix(vec3(1.0), rgb, c.y);
 }
 
 float mandelbrot(vec2 c){
-    vec2 z = vec2(c.x+ + distortMag*sin(t), c.y + distortMag*cos(t));
-	float i = 0.;
-	while(i++ < iter){
+    vec2 z = vec2(c.x + distortMag*sin(t), c.y + distortMag*cos(t));
+    float i = 0.;
+    while (i++ < iter){
         float x = (z.x * z.x - z.y * z.y) + c.x;
         float y = (z.y * z.x + z.x * z.y) + c.y;
-        if(pow(x, 2.) + pow(y, 2.) > 4.){
+        if (pow(x+y, 2.) > 4.){
             break;
         }
-        z = vec2(x,y);
-	}
+        z = vec2(x, y);
+    }
     return i >= iter ? 0. : clamp(i / iter, 0., 1.);
-//	return texture(palette, vec2(i == iter? 0.5 : i/100.), 0.).xyz;
+    //	return texture(palette, vec2(i == iter? 0.5 : i/100.), 0.).xyz;
 }
 
 void main() {
-	vec2 uv = gl_FragCoord.xy / resolution;
-	vec2 c = (gl_FragCoord.xy-.5*resolution.xy)/resolution.y;
+    vec2 uv = gl_FragCoord.xy / resolution;
+    vec2 c = (gl_FragCoord.xy-.5*resolution.xy)/resolution.y;
     c *= scale;
     c += offset/resolution;
     float n = mandelbrot(c);
-    gl_FragColor = vec4(rgb(vec3(hueStart+n*hueRange, 1.-n, clamp(n*1.2,0.,1.))), 1.);
+    float smoothN = smoothstep(.0, brLimit, n);
+    gl_FragColor = vec4(rgb(vec3(hueStart+n*hueRange, 1.-n, smoothN)), 1.);
 }
