26de9225ad5e330157e9853e5c5b1070096fe308
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-09-30 12:59:36 +0200

    removed libs again
    they made github think this project is mostly html which is silly

--- libs/peasycam/src/peasy/PeasyDragHandler.java
+++
@@ -1,5 +0,0 @@
-package peasy;
-
-public interface PeasyDragHandler {
-	public void handleDrag(final double dx, final double dy);
-}
--- libs/peasycam/src/peasy/PeasyWheelHandler.java
+++
@@ -1,5 +0,0 @@
-package peasy;
-
-public interface PeasyWheelHandler {
-	public void handleWheel(final int delta);
-}
