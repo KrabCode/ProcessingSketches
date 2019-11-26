55ceee7fd3f5fba2ddcc513d7202a256ac8740de
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-11-27 00:03:46 +0100

    tentacles

--- src/Anemone2.java
+++ src/Anemone2.java
@@ -2,18 +2,23 @@ import applet.KrabApplet;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jakub 'Krab' Rak on 2019-11-26
  */
 public class Anemone2 extends KrabApplet {
     private PGraphics pg;
+    private ArrayList<Tentacle> tentacles = new ArrayList<Tentacle>();
+    private ArrayList<Tentacle> tentaclesToRemove = new ArrayList<Tentacle>();
 
     public static void main(String[] args) {
         KrabApplet.main("Anemone2");
     }
 
     public void settings() {
-        size(800, 800, P3D);
+//        size(800, 800, P3D);
+        fullScreen(P3D, 1);
     }
 
     public void setup() {
@@ -35,9 +40,42 @@ public class Anemone2 extends KrabApplet {
         pg.rotateX(rot.x);
         pg.rotateY(rot.y);
         pg.rotateZ(rot.z);
+        updateTentacles();
         pg.endDraw();
         image(pg, 0, 0);
         rec(pg);
         gui();
+    }
+
+
+    private void updateTentacles() {
+        group("tentacles");
+        int tentacleCount = sliderInt("count");
+
+        for (Tentacle t : tentacles) {
+            t.update();
+            t.display();
+            if (t.isDead) {
+                tentaclesToRemove.add(t);
+            }
+        }
+        tentacles.removeAll(tentaclesToRemove);
+        tentaclesToRemove.clear();
+    }
+
+
+    class Tentacle{
+        ArrayList<PVector> joints = new ArrayList<PVector>();
+
+        boolean isDead;
+
+        void update(){
+            
+        }
+
+        void display() {
+
+        }
+
     }
 }
