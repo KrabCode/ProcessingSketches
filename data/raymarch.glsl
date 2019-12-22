9ce4d83425fae3447dc5b29e2065cf550fa60b54
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-12-23 00:10:57 +0100

    new 2D noise fn, glowing orb above lunar terrain

--- data/raymarch.glsl
+++ data/raymarch.glsl
@@ -3,7 +3,7 @@
 // cheap fbm https://www.shadertoy.com/view/XslGRr
 // acos(-1) = pi
 
-precision lowp float;
+precision highp float;
 
 uniform vec2 resolution;
 uniform vec3 translate;
@@ -16,8 +16,8 @@ uniform float time;
 uniform float shininess;
 
 uniform int maxSteps = 1000;
-uniform float maxDist = 1000.;
-uniform float surfaceDist = .0001;
+uniform float maxDist = 500.;
+uniform float surfaceDist = 0.00001;
 
 #define pi 3.14159265359
 
@@ -74,57 +74,43 @@ mat2 rotate2d(float angle){
     return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
 }
 
-mediump vec4 permute(in mediump vec4 x){return mod(x*x*34.+x,289.);}
-mediump float snoise(in mediump vec3 v){
-    const mediump vec2 C = vec2(0.16666666666,0.33333333333);
-    const mediump vec4 D = vec4(0,.5,1,2);
-    mediump vec3 i  = floor(C.y*(v.x+v.y+v.z) + v);
-    mediump vec3 x0 = C.x*(i.x+i.y+i.z) + (v - i);
-    mediump vec3 g = step(x0.yzx, x0);
-    mediump vec3 l = (1. - g).zxy;
-    mediump vec3 i1 = min( g, l );
-    mediump vec3 i2 = max( g, l );
-    mediump vec3 x1 = x0 - i1 + C.x;
-    mediump vec3 x2 = x0 - i2 + C.y;
-    mediump vec3 x3 = x0 - D.yyy;
-    i = mod(i,289.);
-    mediump vec4 p = permute( permute( permute(
-    i.z + vec4(0., i1.z, i2.z, 1.))
-    + i.y + vec4(0., i1.y, i2.y, 1.))
-    + i.x + vec4(0., i1.x, i2.x, 1.));
-    mediump vec3 ns = .142857142857 * D.wyz - D.xzx;
-    mediump vec4 j = -49. * floor(p * ns.z * ns.z) + p;
-    mediump vec4 x_ = floor(j * ns.z);
-    mediump vec4 x = x_ * ns.x + ns.yyyy;
-    mediump vec4 y = floor(j - 7. * x_ ) * ns.x + ns.yyyy;
-    mediump vec4 h = 1. - abs(x) - abs(y);
-    mediump vec4 b0 = vec4( x.xy, y.xy );
-    mediump vec4 b1 = vec4( x.zw, y.zw );
-    mediump vec4 sh = -step(h, vec4(0));
-    mediump vec4 a0 = b0.xzyw + (floor(b0)*2.+ 1.).xzyw*sh.xxyy;
-    mediump vec4 a1 = b1.xzyw + (floor(b1)*2.+ 1.).xzyw*sh.zzww;
-    mediump vec3 p0 = vec3(a0.xy,h.x);
-    mediump vec3 p1 = vec3(a0.zw,h.y);
-    mediump vec3 p2 = vec3(a1.xy,h.z);
-    mediump vec3 p3 = vec3(a1.zw,h.w);
-    mediump vec4 norm = inversesqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-    p0 *= norm.x;
-    p1 *= norm.y;
-    p2 *= norm.z;
-    p3 *= norm.w;
-    mediump vec4 m = max(.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.);
-    return .5 + 12. * dot( m * m * m, vec4( dot(p0,x0), dot(p1,x1),dot(p2,x2), dot(p3,x3) ) );
+float noise(vec2 P){
+    //  https://github.com/BrianSharpe/Wombat/blob/master/Perlin2D.glsl
+
+    // establish our grid cell and unit position
+    vec2 Pi = floor(P);
+    vec4 Pf_Pfmin1 = P.xyxy - vec4( Pi, Pi + 1.0 );
+
+    // calculate the hash
+    vec4 Pt = vec4( Pi.xy, Pi.xy + 1.0 );
+    Pt = Pt - floor(Pt * ( 1.0 / 71.0 )) * 71.0;
+    Pt += vec2( 26.0, 161.0 ).xyxy;
+    Pt *= Pt;
+    Pt = Pt.xzxz * Pt.yyww;
+    vec4 hash_x = fract( Pt * ( 1.0 / 951.135664 ) );
+    vec4 hash_y = fract( Pt * ( 1.0 / 642.949883 ) );
+
+    // calculate the gradient results
+    vec4 grad_x = hash_x - 0.49999;
+    vec4 grad_y = hash_y - 0.49999;
+    vec4 grad_results = inversesqrt( grad_x * grad_x + grad_y * grad_y ) * ( grad_x * Pf_Pfmin1.xzxz + grad_y * Pf_Pfmin1.yyww );
+
+    // Classic Perlin Interpolation
+    grad_results *= 1.4142135623730950488016887242097;  // scale things to a strict -1.0->1.0 range  *= 1.0/sqrt(0.5)
+    vec2 blend = Pf_Pfmin1.xy * Pf_Pfmin1.xy * Pf_Pfmin1.xy * (Pf_Pfmin1.xy * (Pf_Pfmin1.xy * 6.0 - 15.0) + 10.0);
+    vec4 blend2 = vec4( blend, vec2( 1.0 - blend ) );
+    return dot( grad_results, blend2.zxzx * blend2.wwyy );
 }
 
-float fbm (vec3 p) {
+float fbm (vec2 p) {
     float value = 0.;
-    float amplitude = 1;
-    float frequency = 0.1;
-    for (int i = 0; i < 3; i++) {
-        float n = snoise(p*frequency);
+    float amplitude = 5;
+    float frequency = 0.03;
+    for (int i = 0; i < 6; i++) {
+        float n = noise(p*frequency);
         value += amplitude * n;
         frequency *= 2.5;
-        amplitude *= 0.5;
+        amplitude *= 0.35;
     }
     return value;
 }
@@ -138,12 +124,18 @@ float sphere(vec3 p, float r){
     return length(p) - r;
 }
 
+float tubularCube(vec3 p, vec2 s){
+    p = abs(p);
+    return max(p.x - s.x, p.y - s.y);
+}
+
+
 float doubleHelix(vec3 p){
-    float r = 1.;
-    float frq = 0.25;
-    float w = 0.6;
-    float helixA = length(vec2(p.x+r*sin(p.z*frq), p.y+r*cos(p.z*frq)))-w;
-    float helixB = length(vec2(p.x+r*sin(pi+p.z*frq), p.y+r*cos(pi+p.z*frq)))-w;
+    float r = 2.;
+    float frq = 0.15;
+    float w = 0.5;
+    float helixA = tubularCube(vec3(p.x+r*sin(p.z*frq), p.y+r*cos(p.z*frq), p.z), vec2(w));
+    float helixB = tubularCube(vec3(p.x+r*sin(pi+p.z*frq), p.y+r*cos(pi+p.z*frq), p.z), vec2(w));
     return min(helixA, helixB);
 }
 
@@ -152,15 +144,19 @@ vec3 repeat(vec3 p, vec3 c){
 }
 
 dist getDistance(vec3 p){
+    float s = sphere(vec3(p)-translate-vec3(0,0,10), 1.);
     int refract = 0;
-    float d = doubleHelix(p);
-    return dist(d, refract);
+    float d = p.y + fbm(p.xz);
+    if(s < d){
+        refract = 1;
+    }
+    return dist(min(d, s), refract);
 }
 
 vec3 getNormal(vec3 p){
     dist d0 = getDistance(p);
     float d = d0.d;
-    vec2 offset = vec2(0.001, 0.);
+    vec2 offset = vec2(0.01, 0.);
     dist d1 = getDistance(p-offset.xyy);
     dist d2 = getDistance(p-offset.yxy);
     dist d3 = getDistance(p-offset.yyx);
@@ -178,9 +174,12 @@ ray raymarch(vec3 rayOrigin, vec3 dir){
         p = rayOrigin+dir*distanceTraveled;
         d = getDistance(p);
         distClosest = min(distClosest, d.d);
+        if(d.d < surfaceDist && d.maxRefractions == 0){
+            break;
+        }
         if(d.d < surfaceDist && refractions < d.maxRefractions){
             vec3 n = getNormal(p);
-            dir = refract(normalize(dir), normalize(n), 0.8);
+            dir = refract(normalize(dir), normalize(n), 0.5);
             rayOrigin = p+dir;
             refractions++;
         }
@@ -221,5 +220,5 @@ vec3 antiAliasRender(vec2 cv){
 
 void main(){
     vec2 cv = (gl_FragCoord.xy-.5*resolution) / resolution.y;
-    gl_FragColor = vec4(render(cv), 1);
+    gl_FragColor = vec4(antiAliasRender(cv), 1.);
 }
