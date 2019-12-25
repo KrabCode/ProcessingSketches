91f4ee3a96d4ce833e02c4188d6ae908ab3fff15
Krabcode <jakubpodtrzitkorak@gmail.com>
2019-12-25 17:06:09 +0100

    snowy particles on raymarch mountain

--- data/raymarch.glsl
+++ data/raymarch.glsl
@@ -15,9 +15,9 @@ uniform float rotate;
 uniform float time;
 uniform float shininess;
 
-uniform int maxSteps = 150;
-uniform float maxDist = 150.;
-uniform float surfaceDist = 0.001;
+const int maxSteps = 100;
+const float maxDist = 500.;
+const float surfaceDist = 0.00000000001;
 
 #define pi 3.14159265359
 
@@ -29,10 +29,15 @@ struct ray{
     float distSum;
 };
 
+struct godray{
+    vec4[maxSteps] hits;
+};
+
 struct dist{
     float d;
     int maxRefractions;
 };
+
 
 vec3 rgb(in vec3 hsb){
     vec3 rgb = clamp(abs(mod(hsb.x*6.0+
@@ -61,6 +66,69 @@ float opIntersection(float d1, float d2) { return max(d1, d2); }
 
 mat2 rotate2d(float angle){
     return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+}
+
+float simplex( vec3 p ){
+    //  https://github.com/BrianSharpe/Wombat/blob/master/SimplexPerlin3D.glsl
+
+    //  simplex math constants
+    const float SKEWFACTOR = 1.0/3.0;
+    const float UNSKEWFACTOR = 1.0/6.0;
+    const float SIMPLEX_CORNER_POS = 0.5;
+    const float SIMPLEX_TETRAHADRON_HEIGHT = 0.70710678118654752440084436210485;    // sqrt( 0.5 )
+
+    //  establish our grid cell.
+    p *= SIMPLEX_TETRAHADRON_HEIGHT;    // scale space so we can have an approx feature size of 1.0
+    vec3 Pi = floor( p + dot( p, vec3( SKEWFACTOR) ) );
+
+    //  Find the vectors to the corners of our simplex tetrahedron
+    vec3 x0 = p - Pi + dot(Pi, vec3( UNSKEWFACTOR ) );
+    vec3 g = step(x0.yzx, x0.xyz);
+    vec3 l = 1.0 - g;
+    vec3 Pi_1 = min( g.xyz, l.zxy );
+    vec3 Pi_2 = max( g.xyz, l.zxy );
+    vec3 x1 = x0 - Pi_1 + UNSKEWFACTOR;
+    vec3 x2 = x0 - Pi_2 + SKEWFACTOR;
+    vec3 x3 = x0 - SIMPLEX_CORNER_POS;
+
+    //  pack them into a parallel-friendly arrangement
+    vec4 v1234_x = vec4( x0.x, x1.x, x2.x, x3.x );
+    vec4 v1234_y = vec4( x0.y, x1.y, x2.y, x3.y );
+    vec4 v1234_z = vec4( x0.z, x1.z, x2.z, x3.z );
+
+    // clamp the domain of our grid cell
+    Pi.xyz = Pi.xyz - floor(Pi.xyz * ( 1.0 / 69.0 )) * 69.0;
+    vec3 Pi_inc1 = step( Pi, vec3( 69.0 - 1.5 ) ) * ( Pi + 1.0 );
+
+    //	generate the random vectors
+    vec4 Pt = vec4( Pi.xy, Pi_inc1.xy ) + vec2( 50.0, 161.0 ).xyxy;
+    Pt *= Pt;
+    vec4 V1xy_V2xy = mix( Pt.xyxy, Pt.zwzw, vec4( Pi_1.xy, Pi_2.xy ) );
+    Pt = vec4( Pt.x, V1xy_V2xy.xz, Pt.z ) * vec4( Pt.y, V1xy_V2xy.yw, Pt.w );
+    const vec3 SOMELARGEFLOATS = vec3( 635.298681, 682.357502, 668.926525 );
+    const vec3 ZINC = vec3( 48.500388, 65.294118, 63.934599 );
+    vec3 lowz_mods = vec3( 1.0 / ( SOMELARGEFLOATS.xyz + Pi.zzz * ZINC.xyz ) );
+    vec3 highz_mods = vec3( 1.0 / ( SOMELARGEFLOATS.xyz + Pi_inc1.zzz * ZINC.xyz ) );
+    Pi_1 = ( Pi_1.z < 0.5 ) ? lowz_mods : highz_mods;
+    Pi_2 = ( Pi_2.z < 0.5 ) ? lowz_mods : highz_mods;
+    vec4 hash_0 = fract( Pt * vec4( lowz_mods.x, Pi_1.x, Pi_2.x, highz_mods.x ) ) - 0.49999;
+    vec4 hash_1 = fract( Pt * vec4( lowz_mods.y, Pi_1.y, Pi_2.y, highz_mods.y ) ) - 0.49999;
+    vec4 hash_2 = fract( Pt * vec4( lowz_mods.z, Pi_1.z, Pi_2.z, highz_mods.z ) ) - 0.49999;
+
+    //	evaluate gradients
+    vec4 grad_results = inversesqrt( hash_0 * hash_0 + hash_1 * hash_1 + hash_2 * hash_2 ) * ( hash_0 * v1234_x + hash_1 * v1234_y + hash_2 * v1234_z );
+
+    //	Normalization factor to scale the final result to a strict 1.0->-1.0 range
+    //	http://briansharpe.wordpress.com/2012/01/13/simplex-noise/#comment-36
+    const float FINAL_NORMALIZATION = 37.837227241611314102871574478976;
+
+    //  evaulate the kernel weights ( use (0.5-x*x)^3 instead of (0.6-x*x)^4 to fix discontinuities )
+    vec4 kernel_weights = v1234_x * v1234_x + v1234_y * v1234_y + v1234_z * v1234_z;
+    kernel_weights = max(0.5 - kernel_weights, 0.0);
+    kernel_weights = kernel_weights*kernel_weights*kernel_weights;
+
+    //	sum with the kernel and return
+    return dot( kernel_weights, grad_results ) * FINAL_NORMALIZATION;
 }
 
 float noise(vec2 P){
@@ -92,14 +160,14 @@ float noise(vec2 P){
 }
 
 float fbm (vec2 p) {
-    float value = 0.;
-    float amplitude = 5;
-    float frequency = 0.03;
-    for (int i = 0; i < 6; i++) {
+    float value = .0;
+    float amplitude = 10;
+    float frequency = 0.05;
+    for (int i = 0; i < 10; i++) {
         float n = noise(p*frequency);
         value += amplitude * n;
         frequency *= 2.5;
-        amplitude *= 0.35;
+        amplitude *= 0.32;
     }
     return value;
 }
@@ -111,6 +179,11 @@ float sdOctahedron(vec3 p, float s){
 
 float sdSphere(vec3 p, float r){
     return length(p) - r;
+}
+
+float sdCube(vec3 p, vec3 b){
+    vec3 q = abs(p) - b;
+    return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
 }
 
 float sdTubularCube(vec3 p, vec2 s){
@@ -137,24 +210,11 @@ vec3 repeat(vec3 p, vec3 c){
 }
 
 dist getDistance(vec3 p){
-    float d = maxDist;
-    float width = 0.025;
-    float height = .05;
-    float baseRadius = 0.9;
-    float step = .15;
-    for (int i = 0; i < 10; i++){
-        vec3 ip = p.xyz;
-        if (mod(i, 2) == 0){
-            ip.xy *= rotate2d(time+i*.5);
-        } else {
-            ip.yz *= rotate2d(time-i*.5);
-        }
-        float outer = sdCappedCylinder(ip, baseRadius+i*step, height);
-        float inner = sdCappedCylinder(ip, baseRadius+i*step-width, height*2);
-        d = min(d, opSubtraction(inner, outer));
-    }
-    float s = sdSphere(p, 0.5);
-    return dist(min(s, d), 0);
+    vec3 center = vec3(0., 10., 1.);
+    float m = 1-length(p-center);
+    float f = fbm(vec2(p.x, p.z));
+    float d = p.y-m*.6-f*.5;
+    return dist(d, 0);
 }
 
 vec3 getNormal(vec3 p){
@@ -224,11 +284,9 @@ vec3 render(vec2 cv){
     float diffuse = getDiffuseLight(r.hit, lightDir, normal, true);
     float specular = getSpecularLight(r.hit, lightDir, rayDirection, normal);
     ray shadow = getShadow(r.hit, normal, lightDir);
-    if(shadow.distSum < maxDist){
-        diffuse *= .2;
+    if (shadow.distSum < maxDist){
+        diffuse *= .95;
         specular *= 0.;
-    }else{
-
     }
     vec3 hsb = vec3(r.hue, r.sat, diffuse*diffuseMag + specular*specularMag);
     vec3 col = rgb(hsb);
@@ -248,5 +306,5 @@ vec3 aaRender(vec2 cv){
 
 void main(){
     vec2 cv = (gl_FragCoord.xy-.5*resolution) / resolution.y;
-    gl_FragColor = vec4(aaRender(cv), 1.);
+    gl_FragColor = vec4(render(cv), 1.);
 }
--- src/Raymarch.java
+++ src/Raymarch.java
@@ -1,30 +1,35 @@
 import applet.KrabApplet;
 import processing.core.PGraphics;
+import processing.core.PVector;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jakub 'Krab' Rak on 2019-12-07
  */
 public class Raymarch extends KrabApplet {
+    private int scaledown = 8;
+    private ArrayList<P> ps = new ArrayList<P>();
+    private ArrayList<P> psBin = new ArrayList<P>();;
     private PGraphics pg;
-    int scaledown = 8;
 
     public static void main(String[] args) {
         KrabApplet.main("Raymarch");
     }
 
     public void settings() {
-        size(800,800, P2D);
+        size(800, 800, P2D);
     }
 
     public void setup() {
         surface.setAlwaysOnTop(true);
-        surface.setLocation(1920-820, 20);
+        surface.setLocation(1920 - 820, 20);
         resetPGraphics();
         frameRecordingDuration *= 2.1;
     }
 
     private void resetPGraphics() {
-        pg = createGraphics(width/scaledown, height/scaledown, P2D);
+        pg = createGraphics(width / scaledown, height / scaledown, P2D);
         pg.beginDraw();
         pg.background(0);
         pg.endDraw();
@@ -32,15 +37,68 @@ public class Raymarch extends KrabApplet {
 
     public void draw() {
         int intendedScaledown = sliderInt("scaledown", 1);
-        if(intendedScaledown > 0 && intendedScaledown != scaledown){
+        if (intendedScaledown > 0 && intendedScaledown != scaledown) {
             scaledown = intendedScaledown;
             resetPGraphics();
         }
         pg.beginDraw();
         rayMarchPass(pg);
+        updateParticles();
         pg.endDraw();
         image(pg, 0, 0, width, height);
         rec(pg);
         gui();
     }
+
+    private void updateParticles() {
+        int intendedCount = sliderInt("count");
+        while (ps.size() < intendedCount) {
+            ps.add(new P());
+        }
+        while (ps.size() > intendedCount) {
+            ps.remove(0);
+        }
+        ps.removeAll(psBin);
+        psBin.clear();
+        for (P p : ps) {
+            p.update();
+        }
+    }
+
+    class P {
+        PVector pos, spd;
+
+        P() {
+            pos = new PVector(
+                    random(-width*2,width),
+                    -50-random(height),
+                    random(width)
+            );
+            spd = new PVector();
+        }
+
+        void update() {
+            PVector acc = sliderXYZ("snow acc").copy();
+            float noiseFreq = slider("noise freq");
+            PVector n = PVector.fromAngle(slider("base angle") + slider("range")*noise(pos.x*noiseFreq, pos.y*noiseFreq, pos.z*noiseFreq));
+            n.mult(slider("noise mag"));
+            PVector g = new PVector(randomGaussian(), randomGaussian(), randomGaussian());
+            g.mult(slider("gauss mag"));
+            acc.add(n);
+            acc.add(g);
+            spd.add(acc);
+            pos.add(spd);
+            spd.mult(slider("drag"));
+            if(pos.y > height || pos.x > width*2 || pos.x < -width){
+                psBin.add(this);
+            }
+            float zDarken = norm(pos.z, 0, width);
+            float w = slider("weight");
+            pg.strokeWeight(constrain(w-zDarken, 1, 10));
+            pg.stroke(255-zDarken*slider("darken"));
+            pg.point(pos.x, pos.y, pos.z);
+        }
+    }
+
+
 }
