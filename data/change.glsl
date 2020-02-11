dfaa06882f5b7ab2346e9ee774f7ce2a4a2ff663
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-02-11 13:46:03 +0100

    hurá

--- data/change.glsl
+++ data/change.glsl
@@ -1,112 +1,102 @@
+precision highp float;
+
 uniform sampler2D texture;
 uniform vec2 resolution;
 uniform float shininess;
 uniform float time;
 uniform vec3 lightDir;
+const int octaves = 6;
 
 #define pi 3.14159
 
-// Permutation polynomial: (34x^2 + x) mod 289
-
-vec4 permute(vec4 x){ return mod(((x*34.0)+1.0)*x, 289.0); }
-float permute(float x){ return floor(mod(((x*34.0)+1.0)*x, 289.0)); }
-vec4 taylorInvSqrt(vec4 r){ return 1.79284291400159 - 0.85373472095314 * r; }
-float taylorInvSqrt(float r){ return 1.79284291400159 - 0.85373472095314 * r; }
-vec4 grad4(float j, vec4 ip){
-    const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);
-    vec4 p, s;
-
-    p.xyz = floor(fract (vec3(j) * ip.xyz) * 7.0) * ip.z - 1.0;
-    p.w = 1.5 - dot(abs(p.xyz), ones.xyz);
-    s = vec4(lessThan(p, vec4(0.0)));
-    p.xyz = p.xyz + (s.xyz*2.0 - 1.0) * s.www;
-
-    return p;
+// noise is from here:
+// https://www.shadertoy.com/view/4dS3Wd by Morgan McGuire @morgan3d!
+// Precision-adjusted variations of https://www.shadertoy.com/view/4djSRW
+float hash(float p) { p = fract(p * 0.011); p *= p + 7.5; p *= p + p; return fract(p); }
+float hash(vec2 p) {vec3 p3 = fract(vec3(p.xyx) * 0.13); p3 += dot(p3, p3.yzx + 3.333); return fract((p3.x + p3.y) * p3.z); }
+float noise(float x) {
+    float i = floor(x);
+    float f = fract(x);
+    float u = f * f * (3.0 - 2.0 * f);
+    return mix(hash(i), hash(i + 1.0), u);
 }
-float snoise(vec4 v){
-    const vec2  C = vec2(0.138196601125010504, // (5 - sqrt(5))/20  G4
-    0.309016994374947451);// (sqrt(5) - 1)/4   F4
-    // First corner
-    vec4 i  = floor(v + dot(v, C.yyyy));
-    vec4 x0 = v -   i + dot(i, C.xxxx);
+float noise(vec2 x) {
+    vec2 i = floor(x);
+    vec2 f = fract(x);
 
-    // Other corners
+    // Four corners in 2D of a tile
+    float a = hash(i);
+    float b = hash(i + vec2(1.0, 0.0));
+    float c = hash(i + vec2(0.0, 1.0));
+    float d = hash(i + vec2(1.0, 1.0));
 
-    // Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
-    vec4 i0;
+    // Simple 2D lerp using smoothstep envelope between the values.
+    // return vec3(mix(mix(a, b, smoothstep(0.0, 1.0, f.x)),
+    //			mix(c, d, smoothstep(0.0, 1.0, f.x)),
+    //			smoothstep(0.0, 1.0, f.y)));
 
-    vec3 isX = step(x0.yzw, x0.xxx);
-    vec3 isYZ = step(x0.zww, x0.yyz);
-    //  i0.x = dot( isX, vec3( 1.0 ) );
-    i0.x = isX.x + isX.y + isX.z;
-    i0.yzw = 1.0 - isX;
-
-    //  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
-    i0.y += isYZ.x + isYZ.y;
-    i0.zw += 1.0 - isYZ.xy;
-
-    i0.z += isYZ.z;
-    i0.w += 1.0 - isYZ.z;
-
-    // i0 now contains the unique values 0,1,2,3 in each channel
-    vec4 i3 = clamp(i0, 0.0, 1.0);
-    vec4 i2 = clamp(i0-1.0, 0.0, 1.0);
-    vec4 i1 = clamp(i0-2.0, 0.0, 1.0);
-
-    //  x0 = x0 - 0.0 + 0.0 * C
-    vec4 x1 = x0 - i1 + 1.0 * C.xxxx;
-    vec4 x2 = x0 - i2 + 2.0 * C.xxxx;
-    vec4 x3 = x0 - i3 + 3.0 * C.xxxx;
-    vec4 x4 = x0 - 1.0 + 4.0 * C.xxxx;
-
-    // Permutations
-    i = mod(i, 289.0);
-    float j0 = permute(permute(permute(permute(i.w) + i.z) + i.y) + i.x);
-    vec4 j1 = permute(permute(permute(permute (
-    i.w + vec4(i1.w, i2.w, i3.w, 1.0))
-    + i.z + vec4(i1.z, i2.z, i3.z, 1.0))
-    + i.y + vec4(i1.y, i2.y, i3.y, 1.0))
-    + i.x + vec4(i1.x, i2.x, i3.x, 1.0));
-    // Gradients
-    // ( 7*7*6 points uniformly over a cube, mapped onto a 4-octahedron.)
-    // 7*7*6 = 294, which is close to the ring size 17*17 = 289.
-
-    vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0);
-
-    vec4 p0 = grad4(j0, ip);
-    vec4 p1 = grad4(j1.x, ip);
-    vec4 p2 = grad4(j1.y, ip);
-    vec4 p3 = grad4(j1.z, ip);
-    vec4 p4 = grad4(j1.w, ip);
-
-    // Normalise gradients
-    vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
-    p0 *= norm.x;
-    p1 *= norm.y;
-    p2 *= norm.z;
-    p3 *= norm.w;
-    p4 *= taylorInvSqrt(dot(p4, p4));
-
-    // Mix contributions from the five corners
-    vec3 m0 = max(0.6 - vec3(dot(x0, x0), dot(x1, x1), dot(x2, x2)), 0.0);
-    vec2 m1 = max(0.6 - vec2(dot(x3, x3), dot(x4, x4)), 0.0);
-    m0 = m0 * m0;
-    m1 = m1 * m1;
-    return 49.0 * (dot(m0*m0, vec3(dot(p0, x0), dot(p1, x1), dot(p2, x2)))
-    + dot(m1*m1, vec2(dot(p3, x3), dot(p4, x4))));
+    // Same code, with the clamps in smoothstep and common subexpressions
+    // optimized away.
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
 }
-float fbm (vec4 p) {
-    float sum = 0.;
-    float amp = 1;
-    float freq = 1;
-    // Loop of octaves
-    for (int i = 0; i < 4; i++) {
-        sum += amp*snoise(p*freq);
-        freq *= 2.;
-        amp *= .5;
-        p += vec4(3.123, 2.456, 1.121, 2.4545);
+float noise(vec3 x) {
+    const vec3 step = vec3(110, 241, 171);
+
+    vec3 i = floor(x);
+    vec3 f = fract(x);
+
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the
+    // incremental change to the 1D based on the 3D -> 1D wrapping
+    float n = dot(i, step);
+
+    vec3 u = f * f * (3.0 - 2.0 * f);
+    return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x),
+    mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y),
+    mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x),
+    mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
+}
+float fbm(float x) {
+    float v = 0.0;
+    float a = 0.5;
+    float shift = float(100);
+    for (int i = 0; i < octaves; ++i) {
+        v += a * noise(x);
+        x = x * 2.0 + shift;
+        a *= 0.5;
     }
-    return sum;
+    return v;
+}
+
+vec3 rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0), 6.0)-3.0)-1.0, 0.0, 1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);  return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
+float fbm(vec2 x) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.50));
+    for (int i = 0; i < octaves; ++i) {
+        v += a * noise(x);
+        x = rot * x * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
+}
+
+float fbm(vec3 x) {
+    float v = 0.0;
+    float a = 0.5;
+    vec3 shift = vec3(100);
+    for (int i = 0; i < octaves; ++i) {
+        v += a * noise(x);
+        x = x * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
 }
 
 struct raypath{
@@ -117,24 +107,21 @@ mat2 rotate2d(float angle){
     return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
 }
 
-float sdSphere(vec3 p){
-    return length(p-vec3(0,0., 3.5))-0.5;
-}
-
-float sdCappedTorus(in vec3 p, in vec2 sc, in float ra, in float rb)
-{
-    p.x = abs(p.x);
-    float k = (sc.y*p.x>sc.x*p.y) ? dot(p.xy,sc) : length(p.xy);
-    return sqrt( dot(p,p) + ra*ra - 2.0*ra*k ) - rb;
-}
 
 float sd(vec3 p){
-    return p.y+1.;
+    vec3 pos = vec3(0,0,0);
+    float radius = 1;
+    float sphere = length(p-pos)-radius;
+
+    if(sphere <= 1.0){
+        return length(p-pos)-(radius);
+    }
+    return sphere;
 }
 
 vec3 getNormal(vec3 p){
     float d = sd(p);
-    vec2 offset = vec2(.00001, 0.);
+    vec2 offset = vec2(.1, 0.);
     float d1 = sd(p-offset.xyy);
     float d2 = sd(p-offset.yxy);
     float d3 = sd(p-offset.yyx);
@@ -143,23 +130,22 @@ vec3 getNormal(vec3 p){
 }
 
 raypath raymarch(vec3 origin, vec3 direction){
-    vec3 p = origin;
+    vec3 p = origin.xyz;
     float distanceTraveled = 0;
     float distanceToScene = sd(origin);
-    int steps = 3000;
-    float surfaceDistance = 0.00001;
-    float maxDistance = 10000;
+    int steps = 1000;
+    float surfaceDistance = 0.001;
+    float maxDistance = 100;
     for(int i = 0; i < steps; i++){
         distanceToScene = sd(p);
         distanceTraveled += distanceToScene;
-        p = direction*distanceTraveled;
+        p = origin+direction*distanceTraveled;
         if(distanceToScene < surfaceDistance || distanceTraveled > maxDistance){
             break;
         }
     }
     return raypath(p);
 }
-
 
 float getDiffuseLight(vec3 p, vec3 lightDir, vec3 normal){
     float diffuseLight = max(dot(normal, -lightDir), 0.0);
@@ -174,7 +160,7 @@ float getSpecularLight(vec3 p, vec3 lightDir, vec3 rayDirection, vec3 normal) {
 
 void main(){
     vec2 cv = (gl_FragCoord.xy-.5*resolution) / resolution.y;
-    vec3 origin = vec3(0, 0, 0);
+    vec3 origin = vec3(0, 0.0,-5.);
     vec3 direction = normalize(vec3(cv, 1));
     raypath path = raymarch(origin, direction);
     vec3 normal = getNormal(path.hit);
