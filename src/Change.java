1c3ea69a6782979d1d519d79f34748b2ff7c1376
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-02-12 16:43:59 +0100

    2D water ripples

--- src/Change.java
+++ src/Change.java
@@ -8,9 +8,14 @@ import java.util.ArrayList;
  * Created by Jakub 'Krab' Rak on 2020-02-06
  */
 public class Change extends KrabApplet {
-    private PGraphics pg;
-    ArrayList<Particle> ps = new ArrayList<Particle>();
+    ArrayList<Float> waterBufferA = new ArrayList<Float>();
+    ArrayList<Float> waterBufferB = new ArrayList<Float>();
+    ArrayList<Float> waterBufferTemp = new ArrayList<Float>();
+    ArrayList<Particle> particles = new ArrayList<Particle>();
+    ArrayList<Particle> particlesToRemove = new ArrayList<Particle>();
+    int waterSize;
     PVector center;
+    private PGraphics pg;
 
     public static void main(String[] args) {
         KrabApplet.main("Change");
@@ -27,12 +32,21 @@ public class Change extends KrabApplet {
         pg.background(0);
         pg.colorMode(HSB, 1, 1, 1, 1);
         pg.endDraw();
-        center = new PVector(width*.5f, height*.5f);
+        center = new PVector(width * .5f, height * .5f);
+        initWater();
     }
 
     public void draw() {
         pg.beginDraw();
+        pg.translate(0, height * .5f);
         alphaFade(pg);
+        int previousWaterSize = waterSize;
+        waterSize = sliderInt("water size", width);
+        if (waterSize != previousWaterSize) {
+            initWater();
+        }
+        updateWater();
+        drawWater();
         updateParticles();
         pg.endDraw();
         image(pg, 0, 0);
@@ -40,59 +54,89 @@ public class Change extends KrabApplet {
         gui();
     }
 
-    private void updateParticles() {
-        int intendedParticleCount = floor(slider("count", 1000));
-        if(button("reset")){
-            ps.clear();
-        }
-        while (ps.size() < intendedParticleCount) {
-            ps.add(new Particle());
-        }
-        while (ps.size() > intendedParticleCount) {
-            ps.remove(0);
-        }
-        for (Particle p : ps) {
-            p.update();
+    private void initWater() {
+        waterBufferA.clear();
+        waterBufferB.clear();
+        waterBufferTemp.clear();
+        for (int i = 0; i < waterSize; i++) {
+            waterBufferA.add(0f);
+            waterBufferB.add(0f);
+            waterBufferTemp.add(0f);
         }
     }
 
-    class Particle{
+    private void updateWater() {
+        for (int i = 1; i < waterSize - 1; i++) {
+            float val = (waterBufferA.get(i - 1) + waterBufferA.get(i + 1)) / slider("neighbour divide", 2)
+                    - waterBufferB.get(i);
+            val *= slider("damp", .9f);
+            waterBufferB.set(i, val);
+        }
+        waterBufferTemp.clear();
+        waterBufferTemp.addAll(waterBufferA);
+        waterBufferA.clear();
+        waterBufferA.addAll(waterBufferB);
+        waterBufferB.clear();
+        waterBufferB.addAll(waterBufferTemp);
+    }
+
+    private void drawWater() {
+        pg.beginShape(TRIANGLE_STRIP);
+        pg.noFill();
+        pg.noStroke();
+        HSBA ceiling = picker("fill ceiling");
+        HSBA floor = picker("fill floor");
+        for (int i = 0; i < waterBufferB.size(); i++) {
+            float x = map(i, 0, waterBufferB.size() - 1, -10, width + 10);
+            float y = waterBufferB.get(i);
+            pg.fill(ceiling.clr());
+            pg.vertex(x, y);
+            pg.fill(floor.clr());
+            pg.vertex(x, height);
+        }
+        pg.endShape();
+    }
+
+    void updateParticles() {
+        int intendedCount = sliderInt("particle count", 100);
+        if (button("reset particles")) {
+            particles.clear();
+        }
+        while (particles.size() < intendedCount) {
+            particles.add(new Particle());
+        }
+        while (particles.size() > intendedCount) {
+            particles.remove(0);
+        }
+        for (Particle p : particles) {
+            p.update();
+        }
+        particles.removeAll(particlesToRemove);
+        particlesToRemove.clear();
+    }
+
+    class Particle {
         PVector pos;
-        PVector spd = new PVector();
+        float weightOffset = random(1);
 
         Particle() {
-            this.pos = new PVector(width*.5f+randomGaussian()*10,height*.5f+randomGaussian()*10);
+            pos = new PVector(random(-width, width * 2), -height * .5f - random(height * .5f));
         }
 
-        void update(){
-            PVector acc = new PVector();
-            acc.add(sliderXY("gravity", 0, 1, 10));
-            acc.add(sliderXY("wind"));
-            spd.add(acc);
-            spd.mult(slider("drag", .9f));
-            enforceConstraints();
-            spd.limit(slider("spd limit", 10));
+        void update() {
+            PVector prev = pos.copy();
+            PVector spd = sliderXY("speed", 0, .5f, 1);
             pos.add(spd);
-            pg.strokeWeight(slider("weight"));
-            pg.stroke(1);
-            pg.point(pos.x, pos.y);
-        }
-
-        private void enforceConstraints() {
-            PVector toCenter = PVector.sub(center, pos);
-            float mag = toCenter.mag();
-            spd.add(toCenter).mult(mag*slider("to center mag"));
-            pos.y = min(pos.y, height);
-            for (Particle p : ps) {
-                if(p.equals(this)){
-                    continue;
-                }
-                if(dist(p.pos.x, p.pos.y, pos.x, pos.y) < slider("repulsion dist")){
-                    PVector awayFromParticle = PVector.sub(pos, p.pos);
-                    float awayMag = awayFromParticle.mag();
-                    spd.add(awayFromParticle.normalize().mult((1/awayMag)*slider("repulsion mag")));
-                }
+            int waterLevelBelowIndex = constrain(floor(map(pos.x, 0, width, 0, waterSize)), 0, waterSize - 1);
+            float waterLevelBelow = waterBufferA.get(waterLevelBelowIndex);
+            if (pos.y > waterLevelBelow) {
+                waterBufferA.set(waterLevelBelowIndex, slider("impact", 10));
+                particlesToRemove.add(this);
             }
+            pg.noStroke();
+            pg.stroke(1);
+            pg.strokeWeight(slider("particle weight") + weightOffset * slider("weight offset"));
+            pg.line(pos.x, pos.y, prev.x, prev.y);
         }
     }
 }
