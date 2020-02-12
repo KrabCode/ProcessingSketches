c05c644c10a5966978a92dcc289eb6e10c42a6bd
Krabcode <jakubpodtrzitkorak@gmail.com>
2020-02-12 22:59:37 +0100

    library reworked due to a glitch, the texture noise doesn't really work yet

--- data/noise/textureNoise.glsl
+++ data/noise/textureNoise.glsl
@@ -1,19 +1,41 @@
+precision highp float;
+
 uniform sampler2D rgbNoise;
 uniform vec2 resolution;
 uniform float time;
 
-// Courtesy of Iñigo Quilez.
-float noise( in vec3 x ){
+//TODO make it work!
+
+// courtesy of Inigo Quilez
+// https://www.shadertoy.com/view/XslGRr
+float noise(vec3 x){
     vec3 p = floor(x);
     vec3 f = fract(x);
     f = f*f*(3.0-2.0*f);
-    vec2 uv = (p.xy+vec2(37.0,17.0)*p.z) + f.xy;
-    vec2 rg = textureLod(rgbNoise, (uv+ 0.5)/256.0, 0.0 ).yx;
-    return mix( rg.x, rg.y, f.z );
+    ivec3 q = ivec3(p);
+    ivec2 uv = q.xy + ivec2(37,239)*q.z;
+    vec2 rg = mix(mix(texelFetch(rgbNoise,(uv)&255,0),
+    texelFetch(rgbNoise,(uv+ivec2(1,0))&255,0),f.x),
+    mix(texelFetch(rgbNoise,(uv+ivec2(0,1))&255,0),
+    texelFetch(rgbNoise,(uv+ivec2(1,1))&255,0),f.x),f.y).yx;
+    return -1.0+2.0*mix( rg.x, rg.y, f.z );
+}
+
+float fbm(vec3 p){
+    float sum = 0.;
+    float amp = 1.;
+    float freq = 1.;
+    for(int i = 0; i < 8; i++){
+        sum += amp*noise(p*freq);
+        amp *= .5;
+        freq *= 2.;
+//        p += vec3(7.123, 5.324, 121.2);
+    }
+    return sum;
 }
 
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
-    vec3 color = vec3(noise(vec3(uv*80.+time*20., 0.)));
+    vec3 color = vec3(fbm(vec3(0., uv)));
     gl_FragColor = vec4(color, 1.);
 }
